861dcf1539ca459f59320d70c481237f78d39953
Hari Ram L <Hariram.L@epssw.com>
2025-08-28 08:48:26 +0530

    feat:inital commit of phase 1

---
+++ Day1/Phase1/src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard';
+import { BugsComponent } from './pages/bugs/bugs';
+import { LoginComponent } from './pages/login/login';
+
+export const routes: Routes = [
+    {path:'',redirectTo:'dashboard',pathMatch:'full'},
+    {path:'dashboard',component:DashboardComponent,title:'Dashboard'},
+    {path:'bugs',component:BugsComponent,title:'Bugs'},
+ 
+    {path:'login',component:LoginComponent,title:'login'},
+    {path:'**',redirectTo:'dashboard'}
+    
+];
---
+++ Day1/Phase1/src/app/app.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone:true,
+  imports: [RouterOutlet, CommonModule, RouterLinkActive, RouterLink],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class AppComponent {
+  protected readonly title = signal('BugTracker');
+}
